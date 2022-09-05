f4acddc3c4749af7622778f4660ab9d1754d1c05
jsalibrahim <jsalibrahim@gmail.com>
2022-09-05 19:39:43 +0300

    test

---
+++ CW1/CW1/CW1App.swift
@@ -0,0 +1,17 @@
+//
+//  CW1App.swift
+//  CW1
+//
+//  Created by Jenan Alibrahim on 05/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct CW1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CW1/CW1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CW1
+//
+//  Created by Jenan Alibrahim on 05/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, my name is Jenan")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
